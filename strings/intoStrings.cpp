f2c9e24934b8d8206321188a568d2854987f90e0
Niharika <niharika86074@gmail.com>
2024-02-19 22:40:31 +0530

    2D array and vector

---
+++ strings/intoStrings.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  char str[5] ={'a','b','\0','d','e'};
+  // for(int i=0; i<5; i++){
+  //   cout<<str[i]<<" ";
+
+  // }
+  cout<<str;
+  char ch= '\0';
+  cout<<ch;
+}
