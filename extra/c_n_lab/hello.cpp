3bbdb49292adca5dc7ed0e2e0b52b9c6cf24567e
Niharika <niharika86074@gmail.com>
2024-02-25 23:27:40 +0530

    hello

---
+++ extra/c_n_lab/hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+// void change(int a[]){
+//   a[0] = 9;
+// }
+
+void change_2d(int arr[3][3]){
+  arr[0][0] = 100;
+}
+int main(){
+  cout<<"hello";
+}
